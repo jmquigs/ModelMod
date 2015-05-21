c2e04dfad14f6b815c3b252070f422fa66dfa112
John Quigley <jmquigs@gmail.com>
2015-05-21 19:01:39 -0400

    Fix(Test): fix test ref data
    - Had to change the test because the vert order changed because I had to reimport from the mmobj - shouldn't change again

--- Test.MMManaged/TestMeshTransform.fs
+++ Test.MMManaged/TestMeshTransform.fs
@@ -28,16 +28,16 @@ let ``MeshTransform: Go ape with the monolith``() =
     let nm = monolith |> MeshTransform.applyMeshTransforms ["rot x 90"; "rot z 45"; "scale 0.5"] [] 
 
     // for visual debugging:    
-    //MeshUtil.WriteObj newMonolith (Path.Combine(tdata, "monolith.OUT.mmobj"))
+    //MeshUtil.WriteObj nm (Path.Combine(Util.TestDataDir, "monolith.OUT.mmobj"))
 
     // check first few positions
     let checkPos p1 ep1 msg = 
-        Check.QuickThrowOnFailure (vecEq p1 ep1 |@ sprintf "%s: %A %A" msg p1 ep1)
+        Check.QuickThrowOnFailure (vecEq p1 ep1 |@ sprintf "%s: got: %A; expected: %A" msg p1 ep1)
 
-    checkPos nm.Positions.[0] (Vec3F(0.f,0.f,0.f)) "mismatch on pos 0"
-    checkPos nm.Positions.[1] (Vec3F(1.414214f,1.414214f,0.f)) "mismatch on pos 1"
+    checkPos nm.Positions.[0] (Vec3F(1.414214f,1.414214f,0.f)) "mismatch on pos 0"
+    checkPos nm.Positions.[1] (Vec3F(1.06066f,1.767767f,0.f)) "mismatch on pos 1"
     checkPos nm.Positions.[2] (Vec3F(-0.3535534f,0.3535534f,0.f)) "mismatch on pos 2"
-    checkPos nm.Positions.[3] (Vec3F(1.06066f,1.767767f,0.f)) "mismatch on pos 3"
+    checkPos nm.Positions.[3] (Vec3F(0.f,0.f,0.f)) "mismatch on pos 3"
 
     
     
