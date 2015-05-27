154860bfcc10dd91e39356d8dfe2de952495265e
John Quigley <jmquigs@gmail.com>
2015-05-27 18:34:06 -0400

    Chore(Doc): add note about how to run tests

--- Test.MMManaged/Program.fs
+++ Test.MMManaged/Program.fs
@@ -1,5 +1,3 @@
-﻿module Tests 
-
-open FsUnit
-open FsCheck
-open NUnit.Framework
+﻿// Usually these tests are run using the NUnit Test Adapter for visual studio (free extension).
+// I haven't set them up to run outside of VS.
+module Tests 
