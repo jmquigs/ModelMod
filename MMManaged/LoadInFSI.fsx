12eda7b787a1842f9940c2a8f1807ce78a054c9d
John Quigley <jmquigs@gmail.com>
2015-05-27 15:15:08 -0400

    Feature(FSI): Add ability to test assembly code from FSI
    - The comment in LoadInFSI explains how to use it.

---
+++ MMManaged/LoadInFSI.fsx
@@ -0,0 +1,32 @@
+﻿(*
+This file can be used to test this assembly in F# Interactive.  
+Create a file called "TestFSI.fsx" with these lines:
+
+#load "LoadInFSI.fsx"
+open ModelMod
+... // your code here
+
+You can then select all text in TestFSI.fsx (Ctrl-A) and hit Alt-enter to 
+send it to FSI to run it.  The advantage of doing it this way is that 
+TestFSI.fsx is ignored by source control, so you can put whatever you want
+in there without the risk of it accidentally getting committed/conflicted.  
+
+This file contains the basic structure of the project, so it is in 
+version control - don't put test code here.
+*)
+
+#I @"..\packages\MonoGame.Framework.WindowsDX.3.3.0.0\lib\net40\"
+#r @"SharpDX.dll"
+#r @"SharpDX.Direct3D9.dll"
+#r @"MonoGame.Framework.dll"
+
+#load "Logging.fs"
+#load "Util.fs"
+#load "CoreTypes.fs"
+
+(*
+To speed up iteration time, the rest of the project files are omitted - 
+use additional #load lines for what you need in TestFSI.fsx.  You may need
+to add more "open ModelMod" lines if you get errors there, because the modules
+assume that everything in ModelMod is in scope.
+*)
