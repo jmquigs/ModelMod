4b648a8cdcb5a256cd7e9f7990b3ec29aea4498e
John Quigley <jmquigs@gmail.com>
2015-09-02 16:52:58 -0400

    Feat(launcher): now displays the profiles found in registry

--- MMLaunch/MainWindow.xaml.fs
+++ MMLaunch/MainWindow.xaml.fs
@@ -5,18 +5,26 @@ open System.Windows
 open FSharp.ViewModule
 open FSharp.ViewModule.Validation
 open System.Windows.Input
+open System.Collections.ObjectModel
 
 open FsXaml
 
 open ViewModelUtils
+open ModelMod
 
 type MainView = XAML<"MainWindow.xaml", true>
 
-
 type MainViewModel() = 
-    inherit ViewModelBase()    
+    inherit ViewModelBase()
 
     let clicks = ref 0
+    do
+        RegConfig.init() // reg config requires init to set hive root
+
+    member x.Profiles = 
+        new ObservableCollection<CoreTypes.RunConfig>(
+            RegConfig.loadAll())
+    
     //member x.Button_Click(sender:Object, e:RoutedEventArgs) = 
     //        printfn "whatever"
 
