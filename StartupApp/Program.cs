0b9c5d7e5dacc03392418b7c7b30fbaf93d822ef
John Quigley <jmquigs@gmail.com>
2015-10-29 18:34:37 -0400

    Chore(startupapp): replace this with a C# version
    - The F# version fails on some systems because it can't locate FSharp.Core (which is in the Bin directory, but I don't want to put this app in there, since it defeats the purpose of having an obvious "Click Me" executable)

---
+++ StartupApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace StartupApp
+{
+    static class Program
+    {
+        // The sole purpose of this app is to have a "ModelMod.exe" file that we can stick 
+        // in the root folder so that the user doesn't have to go into "Bin" and randomly
+        // click executables there.  Obviously an installer would be another way to 
+        // handle this, but installers are generally yucky (especially ones that 
+        // require effing elevated privileges) and I'm too lazy to make one.
+        // This is written in CS so that we don't need an Fsharp.Core outside the bin folder.
+        [STAThread]
+        static void Main()
+        {
+            String[] paths = { @".", @".\Bin" };
+            String target = "MMLaunch.exe";
+
+            String found = null;
+
+            foreach (String p in paths) {
+                var path = Path.Combine(p, target);
+                if (File.Exists(path))
+                {
+                    found = path;
+                    break;
+                }
+            }
+
+            if (found != null)
+            {
+                var proc = new Process();
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.FileName = found;
+                proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(found);
+                proc.Start();
+            }
+        }
+    }
+}
