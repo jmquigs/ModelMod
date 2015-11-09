1d67b0f70102a2293c55b1490d4319379dd29ca2
John Quigley <jmquigs@gmail.com>
2015-11-09 18:07:49 -0500

    Chore(version): 1.0.0.5

--- MMLaunch/AssemblyInfo.fs
+++ MMLaunch/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: GuidAttribute("2ce8e338-7143-4f97-ab39-3e90ca50bdf2")>]
 [<assembly: AssemblyProductAttribute("ModelMod")>]
-[<assembly: AssemblyVersionAttribute("1.0.0.4")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0.4")>]
+[<assembly: AssemblyVersionAttribute("1.0.0.5")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0.5")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0.4"
+    let [<Literal>] Version = "1.0.0.5"
