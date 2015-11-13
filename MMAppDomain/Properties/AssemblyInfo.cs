26527a002ada7d4b7e98d0c8e9975e65d0414435
John Quigley <jmquigs@gmail.com>
2015-11-12 19:52:01 -0500

    Chore(version): 1.0.0.8

--- MMAppDomain/Properties/AssemblyInfo.cs
+++ MMAppDomain/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescriptionAttribute("")]
 [assembly: GuidAttribute("7b59b7f1-5876-4dd3-abc5-ee380144983d")]
 [assembly: AssemblyProductAttribute("ModelMod")]
-[assembly: AssemblyVersionAttribute("1.0.0.7")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.7")]
+[assembly: AssemblyVersionAttribute("1.0.0.8")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.8")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.0.7";
+        internal const string Version = "1.0.0.8";
     }
 }
--- StartupApp/AssemblyInfo.fs
+++ StartupApp/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: GuidAttribute("df438f0d-1e48-42d2-bc4d-7b3500c48515")>]
 [<assembly: AssemblyProductAttribute("ModelMod")>]
-[<assembly: AssemblyVersionAttribute("1.0.0.7")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0.7")>]
+[<assembly: AssemblyVersionAttribute("1.0.0.8")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0.8")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0.7"
+    let [<Literal>] Version = "1.0.0.8"
