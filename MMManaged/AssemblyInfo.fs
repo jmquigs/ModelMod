2931d2cca62bc075ae1c01bdf2b23220e346d4a1
John Quigley <jmquigs@gmail.com>
2016-03-09 14:09:23 -0500

    Version 1.0.0.9

--- MMManaged/AssemblyInfo.fs
+++ MMManaged/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: GuidAttribute("13c62567-ab30-4954-9c47-213bc2a0ab7e")>]
 [<assembly: AssemblyProductAttribute("ModelMod")>]
-[<assembly: AssemblyVersionAttribute("1.0.0.8")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0.8")>]
+[<assembly: AssemblyVersionAttribute("1.0.0.9")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0.9")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0.8"
+    let [<Literal>] Version = "1.0.0.9"
