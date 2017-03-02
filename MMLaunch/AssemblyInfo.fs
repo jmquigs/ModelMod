de4a5565b6e8c7eb041ab8c973bac5cbc704be71
John Quigley <jmquigs@gmail.com>
2017-03-01 21:07:31 -0500

    Version 1.0.0.13

--- MMLaunch/AssemblyInfo.fs
+++ MMLaunch/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: GuidAttribute("2ce8e338-7143-4f97-ab39-3e90ca50bdf2")>]
 [<assembly: AssemblyProductAttribute("ModelMod")>]
-[<assembly: AssemblyVersionAttribute("1.0.0.12")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0.12")>]
+[<assembly: AssemblyVersionAttribute("1.0.0.13")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0.13")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0.12"
+    let [<Literal>] Version = "1.0.0.13"
--- MMManaged/AssemblyInfo.fs
+++ MMManaged/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: GuidAttribute("13c62567-ab30-4954-9c47-213bc2a0ab7e")>]
 [<assembly: AssemblyProductAttribute("ModelMod")>]
-[<assembly: AssemblyVersionAttribute("1.0.0.12")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0.12")>]
+[<assembly: AssemblyVersionAttribute("1.0.0.13")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0.13")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0.12"
+    let [<Literal>] Version = "1.0.0.13"
