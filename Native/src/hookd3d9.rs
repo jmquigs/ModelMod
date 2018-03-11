fc327666c10ff61d3db862c1dd9c55691579acdf
John Quigley <jmquigs@gmail.com>
2018-03-11 15:58:53 -0400

    Native(Cleanup): move d3d9-specific code into hookd3d9

--- Native/src/hookd3d9.rs
+++ Native/src/hookd3d9.rs
@@ -9,6 +9,7 @@ pub use winapi::shared::winerror::{E_FAIL, S_OK};
 use winapi::ctypes::c_void;
 use winapi::um::wingdi::{RGNDATA};
 use util::*;
+use util;
 
 use dnclr::init_clr;
 
@@ -365,6 +366,57 @@ pub unsafe extern "system" fn hook_create_device(THIS: *mut IDirect3D9,
     })          
 }
 
+type Direct3DCreate9Fn = unsafe extern "system" fn(sdk_ver: u32) -> *mut IDirect3D9;
+
+#[allow(unused)]
+#[no_mangle]
+pub extern "system" fn Direct3DCreate9(
+     SDKVersion: u32,
+) -> *mut u64 {
+    match create_d3d9(SDKVersion) {
+        Ok(ptr) => ptr as *mut u64,
+        Err(x) => {
+            write_log_file(&format!("create_d3d failed: {:?}", x));
+            std::ptr::null_mut()
+        }
+    }
+}
+
+fn create_d3d9(sdk_ver:u32) -> Result<*mut IDirect3D9> {
+    let handle = util::load_lib("c:\\windows\\system32\\d3d9.dll")?; // Todo: use GetSystemDirectory
+    let addr = util::get_proc_address(handle, "Direct3DCreate9")?;
+
+    unsafe { 
+        let create:Direct3DCreate9Fn = std::mem::transmute(addr);
+
+        let direct3d9 = (create)(sdk_ver);
+        let direct3d9 = direct3d9 as *mut IDirect3D9;
+        write_log_file(&format!("created d3d: {:x}", direct3d9 as u64));
+
+        // get pointer to original vtable        
+        let vtbl: *mut IDirect3D9Vtbl = std::mem::transmute((*direct3d9).lpVtbl);
+
+        // save pointer to real function
+        let real_create_device = (*vtbl).CreateDevice;
+
+        // unprotect memory and slam the vtable
+        let vsize = std::mem::size_of::<IDirect3D9Vtbl>();
+        let old_prot = util::unprotect_memory(vtbl as *mut c_void, vsize)?;
+
+        (*vtbl).CreateDevice = hook_create_device;
+
+        util::protect_memory(vtbl as *mut c_void, vsize, old_prot)?;
+
+        // create hookstate
+        let hd3d9 = HookDirect3D9 {
+            real_create_device: real_create_device
+        };
+        set_hook_direct3d9(hd3d9);
+
+        Ok(direct3d9)
+    } 
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -418,6 +470,13 @@ mod tests {
         set_hook_device(d3d9device);
     }
 
+    #[test]
+    fn can_create_d3d9() {   
+        let d3d9 = create_d3d9(32);
+        if let Err(x) = d3d9 {
+            assert!(false, format!("unable to create d39: {:?}", x));
+        }
+    }
     #[test]
     fn test_state_copy() {
         set_stub_device();
