ca0a6a6549da290375b658c411910c3c79ee02db
John Quigley <jmquigs@gmail.com>
2020-05-02 17:01:11 -0400

    Prep for native reload - move impl into new subdir

---
+++ Native/HookDX9Impl/rb32.sh
@@ -0,0 +1,7 @@
+if [ "$1" == "profile" ]; then
+BCMD="cargo build --release --features=profile"
+else
+BCMD="cargo build --release"
+fi
+
+$BCMD && cp -v target/release/mm_native.dll /d/Guild\ Wars\ 2/d3d9.dll && RUST_BACKTRACE=1 /d/Guild\ Wars\ 2/Gw2.exe -32
---
+++ Native/HookDX9Impl/rgw1.sh
@@ -0,0 +1,12 @@
+rustup default nightly-i686-pc-windows-msvc
+
+if [ "$1" == "profile" ]; then
+    BCMD="cargo build --release --features=profile"
+else
+    BCMD="cargo build --release"
+fi
+
+# NOTE: use git bash, not WSL
+GPATH="/f"
+
+$BCMD && cp -v target/release/mm_native.dll $GPATH/Guild\ Wars/d3d9.dll && RUST_BACKTRACE=1 $GPATH/Guild\ Wars/Gw.exe
