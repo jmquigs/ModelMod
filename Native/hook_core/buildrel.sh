581bb30ce39bba2af3cf1a3f7db5a639b12a6901
John Quigley <jmquigs@gmail.com>
2021-07-22 16:02:56 -0400

    Use Rust stable for native
    - clean before and after building

--- Native/hook_core/buildrel.sh
+++ Native/hook_core/buildrel.sh
@@ -1,7 +1,7 @@
 set -e
 
-TC_X64=beta-x86_64-pc-windows-msvc
-TC_X32=nightly-i686-pc-windows-msvc
+TC_X64=stable-x86_64-pc-windows-msvc
+TC_X32=stable-i686-pc-windows-msvc
 
 ACTIVE_TC=$(rustup show active-toolchain | awk '{print $1}')
 
@@ -22,12 +22,15 @@ function copy_to_dest {
 }
 
 rustup default $TC_X64
+cargo clean
 cargo build --release
 copy_to_dest modelmod_64
 
 rustup default $TC_X32
+cargo clean
 cargo build --release
 copy_to_dest modelmod_32
 
 rustup default $ACTIVE_TC
+cargo clean
 echo "=== Toolchain reset to $TC_X64"
