4937441115da0715e39863bbe3ef98791c1091d2
John Quigley <jmquigs@gmail.com>
2021-08-06 11:12:16 -0400

    Bump version to 1.1.0.3
    - Skipping a couple version numbers so that it doesn't look too binary

--- MMLaunch/AssemblyInfo.fs
+++ MMLaunch/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("")>]
 [<assembly: GuidAttribute("2ce8e338-7143-4f97-ab39-3e90ca50bdf2")>]
 [<assembly: AssemblyProductAttribute("ModelMod")>]
-[<assembly: AssemblyVersionAttribute("1.1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.1.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.1.0.0"
+    let [<Literal>] Version = "1.1.0.3"
