4f6900ca85cd798eb30b86b0792e849585417579
John Quigley <jmquigs@gmail.com>
2023-03-06 17:40:19 -0500

    Update rb.sh to use alternate path
    - FWIW the name of this script is weird shorthand for "run binary"

--- Native/hook_core/rb.sh
+++ Native/hook_core/rb.sh
@@ -10,7 +10,14 @@ else
 fi
 
 # NOTE: use git bash, not WSL
-GPATH="/f"
+GPATH="/f/Guild Wars 2"
+if [ ! -d "$GPATH" ]; then
+    GPATH="/c/Guild Wars 2"
+fi
+if [ ! -d "$GPATH" ]; then
+    echo "Can't find game"
+    exit 1
+fi
 
 # select d3d 9 or 11 via first argument, file goes in a different place in each case
 if [ "$1" == "11" ]; then
@@ -19,4 +26,4 @@ else
     DEST=bin64/d3d9.dll
 fi
 
-$BCMD && cp -v target/release/hook_core.dll $GPATH/Guild\ Wars\ 2/$DEST && RUST_BACKTRACE=1 $GPATH/Guild\ Wars\ 2/Gw2-64.exe
+$BCMD && cp -v target/release/hook_core.dll "$GPATH/$DEST" && RUST_BACKTRACE=1 "$GPATH/Gw2-64.exe"
